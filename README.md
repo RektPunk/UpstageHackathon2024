93692e50611373116b4a21b6dc2863a56e08faa6
RektPunk <rektpunk@gmail.com>
2024-08-26 13:45:17 +0900

    change README, index page

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Welcome to Global AI Hackathon!
+# Global AI Hackathon at a Glance: Data & Discoveries
+
+## Overview
+
+This project showcases the analysis of participant data and code submissions from our recent Hackathon event, "AGI for Work". It includes a dynamic dashboard that visualizes key statistics about the participants and a comprehensive document that extracts and presents insights from the code submitted by the participants.
+
+## Features
+1. **Participant Data Dashboard**: 
+   - Visual representation of various statistical metrics related to the hackathon participants.
+   - Interactive charts and graphs that provide insights into demographics, skills, APIs, and other relevant data.
+   - Easy navigation to explore different aspects of the participant data.
+
+2. **Code Submission Insights**:
+   - Detailed analysis of the code submitted during the hackathon.
+   - Key insights and patterns identified from the submissions.
+   - Summary of best practices, innovative solutions, and common challenges faced by participants.
+
+## Acknowledgments
+- A special thanks to all hackathon participants for their incredible work and contributions.
+- Thanks to the Upstage for making this hackathon a success.
--- solar_report/pages/index.py
+++ solar_report/pages/index.py
@@ -30,7 +30,7 @@ def _time_data() -> rx.Component:
             rx.icon("info", size=20),
             content=f"{(datetime.datetime.now() - datetime.timedelta(days=60)).strftime('%b %d, %Y')} - {datetime.datetime.now().strftime('%b %d, %Y')}",
         ),
-        rx.text("Last 60 days", size="4", weight="medium"),
+        rx.text("Pre, during, post Hackathon", size="4", weight="medium"),
         align="center",
         spacing="2",
         display=["none", "none", "flex"],
@@ -49,16 +49,11 @@ def tab_content_header() -> rx.Component:
 
 @template(route="/", title="Overview", on_load=StatsState.randomize_data)
 def index() -> rx.Component:
-    """The overview page.
-
-    Returns:
-        The UI for the overview page.
-    """
     return rx.vstack(
-        rx.heading(f"Welcome", size="9"),
-        rx.text("greetings & introduction on hackathon blah blah"),
-        rx.heading(f"Participants", size="8"),
-        rx.text("blah blah"),
+        rx.heading("Welcome to the Global AI Hackathon at a Glance", size="8"),
+        rx.markdown("This document provides a comprehensive overview of the **Global AI Hackathon**, capturing key insights and data from the event. The hackathon brought together AI enthusiasts from across the globe, and this summary aims to highlight the most important aspects of their participation."),
+        rx.heading("Participants", size="7"),
+        rx.markdown("An analysis of the participant data from our recent global hackathon is presented. We had an incredible turnout, with participants joining us from all around the world. Below, you'll find key statistics that highlight the diversity and reach of this event, including the total number of participants and the number of countries represented. These figures not only showcase the scale of our hackathon but also emphasize the global community that came together to innovate and collaborate in the field of AI."),
         participants_stats_cards(),
         rx.grid(
             card(
@@ -80,8 +75,17 @@ def index() -> rx.Component:
             ],
             width="100%",
         ),
-        rx.heading("Submission", size="8"),
-        rx.text("there are blah blah"),
+        rx.heading("Submission Statistics", size="7"),
+        rx.markdown("""
+            We delve into the key metrics related to the projects submitted during the Global AI Hackathon. 
+            Participants, organized into teams, tackled various challenges across different tracks, showcasing their creativity and technical expertise.
+            We received submissions from numerous teams, each contributing unique projects. 
+        """),
+        rx.markdown("""
+            Participants had the opportunity to choose from several tracks, each focusing on different aspects of LLM.
+            An analysis of the tools and frameworks that were most frequently used by teams in their projects. 
+            This includes a summary of the programming languages, libraries, and APIs that powered the innovative solutions submitted.
+        """),
         submission_stats_cards(),
         rx.grid(
             card(
@@ -120,8 +124,12 @@ def index() -> rx.Component:
             ],
             width="100%",
         ),
-        rx.heading(f"API usage", size="8"),
-        rx.text("There are blah blah"),
+        rx.heading(f"API usage", size="7"),
+        rx.markdown("""
+            We focuse on the usage of APIs throughout the **Global AI Hackathon**, providing insights into how participants leveraged external services to power their projects.
+            We tracked the volume of API calls made by participants before, during, and after the hackathon. This analysis highlights the surge in activity as the event progressed, showing how teams increasingly relied on APIs to build and refine their solutions.
+            Each track presented unique challenges that required different sets of APIs. We break down which APIs were most popular within each track, offering insights into the specific technologies that teams favored to address their chosen challenges.
+        """),
         api_stats_cards(),
         rx.grid(
             card(
